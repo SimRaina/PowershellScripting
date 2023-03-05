e551999b015fa73ec37ede655958e6d64bbf2092
Sim Raina QA <simran.pre90@gmail.com>
2023-03-05 14:41:39 +0530

    Adding Basic Script from Session 1

---
+++ BasicTest.ps1
@@ -0,0 +1,16 @@
+﻿cls  # clear console output screen
+$path = Get-Location  # returns curent path of the script
+echo $path
+
+# Check if a folder exist. If not then create it.
+
+$foldertocheck = ".\NewFolder" # $foldertocheck = "$path\NewFolder"
+
+if(Test-Path $foldertocheck){
+  Write-Host "Folder exist"
+}
+
+else{
+ New-Item $foldertocheck -ItemType Directory
+ Write-Host "Folder Created" 
+}
---
+++ DeleteFiles.ps1
@@ -0,0 +1,31 @@
+﻿cls
+$path = Get-Location
+$excluded_file = "DoNotDelete.txt"
+$flag = 0
+$count = 0
+
+function WriteLog{
+Param ([String]$LogMsg)
+$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+$LogMessage = "$Stamp :: $LogMsg"
+Add-Content $path\log.txt -value $LogMessage
+}
+
+$Files = Get-ChildItem -Path $path -File -Recurse -Include *.txt | where{$_.name -ne $excluded_file}
+    WriteLog "===========Script Started =======" 
+    foreach ($File in $Files){
+     $flag = 1
+     if ($File -ne $Null){
+      Remove-Item $File.FullName -Force -Verbose
+      WriteLog "Deleted => $File"
+      $count++
+     }
+   }
+
+   if($flag -eq 0){
+    WriteLog "No File To Delete" 
+   }
+
+   else{
+       WriteLog " Script ended and deleted $count file(s)"
+       }
