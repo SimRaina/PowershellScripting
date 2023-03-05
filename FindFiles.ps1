963014a6c3f204988b2d6178f6951a94cdaa4b47
Sim Raina QA <simran.pre90@gmail.com>
2023-03-05 16:37:59 +0530

    Added Get-ChildItem CmdLet

---
+++ FindFiles.ps1
@@ -0,0 +1,7 @@
+﻿# Find the files inside a specific folder
+cls
+$path = ".\NewFolder"
+
+
+$Files = Get-ChildItem -Path $path -File -Recurse -Include *.txt -Exclude *.log # returns files in that path
+echo $Files
