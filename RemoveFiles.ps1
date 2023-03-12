6dc256c635f3d81aa86038fdafcddde177b6e559
Sim Raina QA <simran.pre90@gmail.com>
2023-03-12 16:16:15 +0530

    Powershell Sessions - Deleting the files

---
+++ RemoveFiles.ps1
@@ -0,0 +1,19 @@
+﻿# Find the files inside a specific folder
+cls
+$path = ".\NewFolder"
+$excludedFile = "DoNotDelete.txt"
+
+
+$Files = Get-ChildItem -Path $path -File -Recurse -Include *.txt -Exclude $excludedFile # returns files in that path
+
+
+if($Files -ne $Null){
+foreach($File in $Files){
+
+Remove-Item $File -Force -Verbose
+
+}
+}
+else{
+Write-Host "No File to Delete"
+}
